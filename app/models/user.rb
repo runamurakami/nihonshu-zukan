10af03cd7fc0b61806b2fadd7bb2edb86a1a66a3
村上瑠菜 <runa@murakaminoMacBook-Air.local>
2025-11-03 14:58:13 +0900

    Userモデルからrecoverableを削除・routes.rb で skip: [:passwords] を設定し、パスワードリセット機能を無効化

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
+  # パスワードリセット機能無効化のため :recoverable を削除
 end
