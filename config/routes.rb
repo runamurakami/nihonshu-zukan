cd9f30de158f8bfe3b04e8392dda77642705215c
村上瑠菜 <runa@murakaminoMacBook-Air.local>
2025-11-05 16:00:32 +0900

    ユーザー登録・ログイン機能の基本実装（controller, view, name追加など）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # パスワードルートを無効化
-  devise_for :users, skip: [ :passwords ]
+  devise_for :users, skip: [ :passwords ], controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
