c6a3d09e2edd43783f6997f6ab2ab62ae833ef8e
村上瑠菜 <runa@murakaminoMacBook-Air.local>
2026-03-01 21:22:44 +0900

    トップページのレイアウトをタブ切り替え形式に変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,8 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!, only: [ :top ]
+
+  def landing
+    redirect_to top_path if user_signed_in?
+  end
   def top; end
 end
