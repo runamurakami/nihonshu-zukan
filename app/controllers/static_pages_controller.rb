54e33d80ea4e990de287c2b8c59826fe1749c132
村上瑠菜 <runa@murakaminoMacBook-Air.local>
2026-03-06 21:45:03 +0900

    chart.jsを導入しサンプルグラフを表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,14 @@ class StaticPagesController < ApplicationController
   end
   def top
     @recent_sakes = current_user.sakes.order(created_at: :desc).limit(5)
+
+    months = (0..5).map { |i| i.months.ago.beginning_of_month }.reverse
+    counts = current_user.sakes
+      .where(created_at: 6.months.ago.beginning_of_month..Time.current)
+      .group("DATE_TRUNC('month', created_at)")
+      .count
+    @sake_counts_by_month = months.map do |month|
+      [ month, counts[month] || 0 ]
+      end.to_h
   end
 end
